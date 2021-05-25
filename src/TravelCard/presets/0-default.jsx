5c0f4b72958297a54c7e743cd767579bc20fb26e
unknown <paris@digitalus.com>
2021-05-25 13:28:42 -0400

    added price and rating fields to card header, changed the location of like icon

--- src/TravelCard/presets/0-default.jsx
+++ src/TravelCard/presets/0-default.jsx
@@ -7,21 +7,24 @@ import CardActions from "../../CardActions/CardActions";
 import Avatar from "../../Avatar/Avatar";
 import Typography from "../../Typography/Typography";
 import Icon from "../../Icon/Icon";
-import IconButton from "../../IconButton/IconButton";
 import CardM from '@material-ui/core/Card';
 
 export default (
   <TravelCard uxpId="travelcard">
     <CardMedia uxpId="1.1"
       image="https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
-      style={{"height": "200px"}}
     />
+     <CardActions disableActionSpacing uxpId="1.4">
+      <IconButton aria-label="Add to favorites" uxpId="1.4.1">
+        <Icon uxpId="1.4.1.1">favorite</Icon>
+      </IconButton>
+    </CardActions>
     <CardHeader
       uxpId="1.2"
       avatar={<Avatar aria-label="Destination" uxpId="1.1.1">R</Avatar>}
-      action={<IconButton uxpId="1.1.2"><Icon uxpId="1.1.2.1">more_vert</Icon></IconButton>}
       title="Savannah"
-      subheader="September 14, 2016"
+      price="$500"
+      rating="4.8/5"
     />
     <CardContent uxpId="1.3">
       <Typography component="p" uxpId="1.3.1">
@@ -30,13 +33,5 @@ export default (
         mussels, if you like.
       </Typography>
     </CardContent>
-    <CardActions disableActionSpacing uxpId="1.4">
-      <IconButton aria-label="Add to favorites" uxpId="1.4.1">
-        <Icon uxpId="1.4.1.1">favorite</Icon>
-      </IconButton>
-      <IconButton aria-label="Share" uxpId="1.4.2">
-        <Icon uxpId="1.4.2.1">share</Icon>
-      </IconButton>
-    </CardActions>
   </TravelCard>
 );
